1a6a7cc5a782fce3b2a5f6940f3af9ce8dd57d6a
Juan Diego <juandiegosamp@hotmail.com>
2026-05-04 23:39:11 -0500

    feat: update SonarQube build step to skip tests and add application test properties

--- couriersync-backend/src/test/java/com/ep18/couriersync/backend/CouriersyncBackendApplicationTests.java
+++ couriersync-backend/src/test/java/com/ep18/couriersync/backend/CouriersyncBackendApplicationTests.java
@@ -3,13 +3,13 @@ package com.ep18.couriersync.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class CouriersyncBackendApplicationTests {
-
 	@Test
 	void contextLoads() {
-	}
-
+	}	
 }
